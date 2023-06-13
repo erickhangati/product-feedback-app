55e97ff4cb986e8e276aded62ca1ee68510c1385
Eric Khangati <ekhangati@gmail.com>
2023-06-13 20:08:53 +0300

    Added Next Auth Secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -39,6 +39,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXT_PUBLIC_SECRET,
 };
 
 export default NextAuth(authOptions);
